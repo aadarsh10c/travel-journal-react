9c60e00cdb0abfb2e0f8dade19db02bd2abf326e
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-24 18:26:27 +0530

    Working on NAvigation componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Navigation from './component/Navigation'
+import Content from './component/Content'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      TEST
+      <Navigation  />
+      <Content />
     </div>
   )
 }
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navigation(){
+    return (
+        <nav className="header">
+            <a href="#" className="header__link">
+                <i class="fa-solid fa-earth-asia"></i>
+                <h4>my travel journal</h4>
+            </a>
+        </nav>
+    );
+}
